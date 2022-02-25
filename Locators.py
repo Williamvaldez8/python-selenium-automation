63446c44232795a924248468c6b4a9e0b1126e67
williamvaldez8 <williamavaldez@gmail.com>
2022-02-25 12:45:58 -0700

    updated the chromedriver

---
+++ Locators.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# init driver
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+driver.find_element(by.id, id="twotabsearchtextbox")
+
+drinver.find_element(by. path)
